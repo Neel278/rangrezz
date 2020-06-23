c7c6de8663323f200bb5164039a120addc6d466a
Neel278 <neelthakkar2001@gmail.com>
2020-06-23 12:53:19 +0530

    username validation updated

--- app/Http/Controllers/SettingsController.php
+++ app/Http/Controllers/SettingsController.php
@@ -15,15 +15,15 @@ class SettingsController extends Controller
     public function editProfile(Request $request)
     {
         // dd(request()->input());
-        $validatedAttr = request()->validate([
+        // $validatedAttr = ;
+        // dd($validatedAttr);
+        $user = User::find(auth()->user()->id)->first();
+        $user->update(request()->validate([
             'lastname' => ['required', 'string', 'max:255'],
             'firstname' => ['required', 'string', 'max:255'],
             'address' => ['required', 'string', 'max:255'],
-            'username' => ['string', 'required', 'max:255', 'alpha_dash', Rule::unique('users')->ignore(auth()->user()->id)],
-        ]);
-        // dd($validatedAttr);
-        $user = User::find(auth()->user()->id)->first();
-        $user->update($validatedAttr);
+            'username' => ['string', 'required', 'max:255', 'min:6','alpha_dash', Rule::unique('users')->ignore(auth()->user()->id)],
+        ]));
         return redirect()->back();
         
     }
